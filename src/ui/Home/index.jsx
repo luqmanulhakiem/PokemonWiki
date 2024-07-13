acbe908fe4c68dfe3f4d39e8a348862214a9bf73
Luqman Project <luqmanulhakiem7@gmail.com>
2024-07-13 12:07:26 +0700

    package: add axios, feat: getData Pokemon

--- src/ui/Home/index.jsx
+++ src/ui/Home/index.jsx
@@ -1,11 +1,35 @@
-import { Button, Image, ScrollView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Button, FlatList, Image, ScrollView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Avatar } from '../../assets/svg'
 import { Homestyles } from './style'
+import { BASE_URL } from '../../data/utils/apiService'
+import axios from 'react-native-axios'
 
 const HomeScreen = ({navigation}) => {
+
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  async function getData(){
+      try {   
+        const res = await axios.get(`${BASE_URL}`);
+        const result = {
+            data: res.data.results,
+        };
+        if (res) {
+            setData(res.data.results)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
   return (
     <View style={Homestyles.main}>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -22,49 +46,30 @@ const HomeScreen = ({navigation}) => {
         <View style={Homestyles.searchBar}>
           <TextInput
             style={Homestyles.inputSearch}
+            value={inputValue}
+            onChangeText={(text) => setInputValue(text)}
             placeholder='Search Pokemon...'
           />
-          <TouchableOpacity style={Homestyles.btnSearch} onPress={() => navigation.navigate("SearchScreen")}>
+          <TouchableOpacity style={Homestyles.btnSearch} onPress={() => navigation.navigate("SearchScreen", {inputValue})}>
             <FontAwesomeIcon style={{color: 'white'}} icon={faSearch} />
           </TouchableOpacity>
         </View>
         <View style={Homestyles.grid}>
-          <TouchableOpacity style={Homestyles.cardItem} onPress={() => navigation.navigate("DetailScreen")}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={Homestyles.cardItem}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
-            <Text style={Homestyles.cardText}>Nama Pokemon</Text>
-          </TouchableOpacity>
+          {data.length > 0 ?
+          data.map((item, key) => {
+              return (
+                <TouchableOpacity key={key} style={Homestyles.cardItem} onPress={() => navigation.navigate("DetailScreen")}>
+                  <Image style={Homestyles.cardImage} source={{uri: `https://img.pokemondb.net/artwork/${item.name}.jpg`}} />
+                  <Text style={Homestyles.cardText}>{item.name}</Text>
+                </TouchableOpacity>
+              );
+            })
+            :
+            <View style={Homestyles.noData}>
+              <Text style={Homestyles.fontfamily}>Pokemon tidak ditemukan</Text>
+            </View>
+          }
+          
         </View>
       </ScrollView>
     </View>
--- src/ui/Search/index.jsx
+++ src/ui/Search/index.jsx
@@ -2,12 +2,13 @@ import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Homestyles } from '../Home/style'
 
-const SearchScreen = () => {
+const SearchScreen = ({route}) => {
+  const { inputValue } = route.params;
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={Homestyles.main}>
       <View style={Homestyles.grid} >
           <TouchableOpacity style={Homestyles.cardItem} onPress={() => navigation.navigate("DetailScreen")}>
-            <Image style={Homestyles.cardImage} source={{uri: 'https://img.pokemondb.net/artwork/mewtwo.jpg'}} />
+            <Image style={Homestyles.cardImage} source={{uri: `https://img.pokemondb.net/artwork/${inputValue}.jpg`}} />
             <Text style={Homestyles.cardText}>Nama Pokemon</Text>
           </TouchableOpacity>
           <TouchableOpacity style={Homestyles.cardItem}>
